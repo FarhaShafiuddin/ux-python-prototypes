fd0139d5bab284f61dcd50041213d2d5972fea87
FarhaShafiuddin <shafiuddinfarha@gmail.com>
2025-12-30 22:40:42 -0600

    Add Accessible Form Prototype app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Test App", layout="wide")
+st.title("✅ Streamlit is working!")
+st.write("If you see this, your app is connected and running fine.")
+
